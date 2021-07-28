57a5790bb1eb1d90ba10714edf317cec4701c4f0
OwczarekGit <null>
2021-07-28 19:44:37 +0000

    Initial commit

---
+++ minimal/.local/share/sysstat/sysstat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+WORK_DIR="$HOME/.local/share/sysstat"
+
+OUTPUT_FILE="$WORK_DIR/output"
+mkdir -p $WORK_DIR
+
+
+getIP(){
+	local IP=$(nmcli -p | grep inet4 | sed "s/inet4//g; s/ //g; s/\t//g; s/\/.*//g")
+	echo $IP
+}
+
+getDate(){
+	local date=$(date +%d.%m.%Y)
+	echo $date
+}
+
+getTime(){
+	local time=$(date +%H:%M)
+	echo $time
+}
+
+IP_ADDRESS=$(getIP)
+DATE=$(getDate)
+TIME=$(getTime)
+
+while true; do
+	echo " IP: $IP_ADDRESS | $DATE | $TIME " > $OUTPUT_FILE
+	xsetroot -name "$(cat $OUTPUT_FILE)"
+	sleep 1
+done
+
+
