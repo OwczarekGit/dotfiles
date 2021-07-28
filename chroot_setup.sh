231080ea87fe3056d529693d530ab63780de38fa
OwczarekGit <null>
2021-07-28 21:29:51 +0000

    Renamed script

--- chroot_setup.sh
+++ chroot_setup.sh
@@ -5,6 +5,10 @@ echo "This script should be run as root after running arch-chroot."
 echo "If you don't meet that requirement please CTRL+C now, else wait couple seconds."
 sleep 5
 
+# Setup ntp and stuff.
+timedatectl set-ntp true
+hwclock --systohc
+
 # Create and setup user.
 echo "Please specify your user name:"
 read user
@@ -23,15 +27,19 @@ read hostname
 echo "$hostname" > /etc/hostname
 
 # Installing paru aur helper.
+pacman -Syy
 pacman -S git --noconfirm
 git clone "https://aur.archlinux.org/paru-bin.git"
 cd paru-bin && makepkg -si
 
 # Set the locale to the one i prefer.
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+
 cp locale.conf /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen
 echo "KEYMAP=pl" > /etc/vconsole.conf
+echo "a4" >> /etc/papersize
 
 echo 'MAKEFLAGS="-j4"' >> /etc/makepkg.conf
 
@@ -43,8 +51,12 @@ echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 
 echo "BottomUP" >> /etc/paru.conf
 
+paru -Syy
+
 # Enable system services.
 systemctl enable NetworkManager bluetooth tlp
 
 # Enable user services.
 systemctl enable --user pipewire-pulse pipewire
+
+echo "Done now install boot loader, reboot, and run post_reboot script."
