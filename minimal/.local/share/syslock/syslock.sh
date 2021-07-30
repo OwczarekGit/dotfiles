c35fc55458fedbf0a06fdd00baa9a477953215b3
OwczarekGit <null>
2021-07-30 14:25:18 +0200

    Set resize to more common resolution.

--- minimal/.local/share/syslock/syslock.sh
+++ minimal/.local/share/syslock/syslock.sh
@@ -8,6 +8,6 @@ cd $WORKDIR
 
 maim -q -m 6 $SCREEN
 convert $SCREEN -scale 20% -blur 0x16 $SCREEN
-convert $SCREEN -resize 1280x800 $SCREEN
+convert $SCREEN -resize 1920x1080 $SCREEN
 i3lock -e -c "#000000" -i $SCREEN
 rm $SCREEN
