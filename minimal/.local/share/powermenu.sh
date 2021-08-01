1d862137f9f5d47ad27fa659200ffce7c966ae8c
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-08-02 00:13:32 +0200

    Added powermenu Super+shift+Backspace, Removed shortcuts for shutting down they were to easy to press by accident.

---
+++ minimal/.local/share/powermenu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#printf "Cancel\nLock\nSuspend\nShutdown\nReboot" | dmenu -p "Option:" -l 10
+SELECTION=$(printf "Cancel\nLock\nSuspend\nShutdown\nReboot" | rofi -show -dmenu -p "Option" -l 5)
+
+case $SELECTION in
+
+	Cancel)
+		exit
+		;;
+
+	Lock)
+		~/.local/share/syslock/syslock.sh
+		;;
+
+	Suspend)
+		~/.local/share/syslock/syslock.sh
+		systemctl suspend
+		;;
+
+	Shutdown)
+		systemctl poweroff
+		;;
+
+	Reboot)
+		systemctl reboot
+		;;
+esac
+
+
