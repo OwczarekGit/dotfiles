f83a775fc15dfa7ab8006f1cb328607701fc5499
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-09-01 02:04:01 +0200

    Added emboss to screen lock, it looks cool and makes things harder to distinguish.

--- minimal/.local/share/syslock/syslock.sh
+++ minimal/.local/share/syslock/syslock.sh
@@ -7,7 +7,7 @@ mkdir -p $WORKDIR
 cd $WORKDIR
 
 maim -q -m 6 $SCREEN
-convert $SCREEN -scale 20% -blur 0x16 $SCREEN
+convert $SCREEN -scale 20% -blur 0x16 -emboss 10 $SCREEN
 convert $SCREEN -resize 1920x1080 $SCREEN
 i3lock -e -c "#000000" -i $SCREEN
 rm $SCREEN
