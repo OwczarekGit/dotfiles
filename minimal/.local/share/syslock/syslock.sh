8ba290f477b31b75ecae2fa9794c6cecfd1819a0
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-09-03 06:09:58 +0200

    Enable dpms when the screen is locked.

--- minimal/.local/share/syslock/syslock.sh
+++ minimal/.local/share/syslock/syslock.sh
@@ -6,8 +6,12 @@ SCREEN="$WORKDIR/lockimg.png"
 mkdir -p $WORKDIR
 cd $WORKDIR
 
-maim -q -m 6 $SCREEN
-convert $SCREEN -scale 20% -blur 0x16 -emboss 10 $SCREEN
-convert $SCREEN -resize 1920x1080 $SCREEN
-i3lock -e -c "#000000" -i $SCREEN
+xset dpms 10 10 10
+
+maim -d 0.5 -q -m 6 $SCREEN
+convert $SCREEN -scale 20%  -blur 0x16 -emboss 10 $SCREEN
+convert $SCREEN -resize 1366x768 $SCREEN
+i3lock -n -e -c "#000000" -i $SCREEN
 rm $SCREEN
+
+xset dpms 0 0 0
